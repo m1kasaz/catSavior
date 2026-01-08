18434d37abd0736d854723ea4c845e91b19fb165
m1kasaz <1700809114@qq.com>
2026-01-08 17:19:27 +0800

    短链接分享

---
+++ catcat/src/main/java/com/qin/catcat/unite/mapper/ShotLinkShareMapper.java
@@ -0,0 +1,17 @@
+package com.qin.catcat.unite.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qin.catcat.unite.popo.entity.ShotLink;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Description 短链接分享Mapper
+ *
+ * @Author liuyun
+ * @Version 1.0
+ * @Since 2025-01-18 22:47
+ */
+@Mapper
+public interface ShotLinkShareMapper extends BaseMapper<ShotLink> {
+
+}
