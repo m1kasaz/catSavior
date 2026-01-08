ddffed73ee701bd907808737e3cac20acb1a72bf
m1kasaz <1700809114@qq.com>
2026-01-08 17:42:29 +0800

    定时任务处理器

---
+++ catcat/src/main/java/com/qin/catcat/unite/mapper/DonateMapper.java
@@ -0,0 +1,10 @@
+package com.qin.catcat.unite.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qin.catcat.unite.popo.entity.Donate;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DonateMapper extends BaseMapper<Donate>{
+    
+}
